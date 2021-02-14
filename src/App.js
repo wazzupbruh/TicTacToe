dd5e8c8ee455258a12fe7e7c2af19b54164c66a3
Vlad <sw1zzyotec@gmail.com>
2021-02-14 20:02:13 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import Board from "./components/Board"
+import React from 'react';
+import Board from './components/Board';
+
+import './styles/root.scss';
 
 // eslint-disable-next-line arrow-body-style
 const App = () => {
-  return(
-    <div>
+  return (
+    <div className="app">
       <h1>TIC TAC TOE</h1>
       <Board />
     </div>
